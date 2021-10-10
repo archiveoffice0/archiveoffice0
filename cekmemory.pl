38c45bc4cbc659f615480d4b234d34913d72dead
archiveoffice0 <92211528+archiveoffice0@users.noreply.github.com>
2021-10-10 08:52:19 +0700

    Create cekmemory.pl

---
+++ cekmemory.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+$result =`cat /proc/meminfo | grep MemFree:`;
+@val = split(/:/, $result);
+$val[1] =~ s/^\s+|\s+$//g;
+@v = split(/ /, $val[1]);
+print "$v[0]\n";
+if($v[0] < 500000)
+{
+        print "less than 500000";
+        $cmd = "/etc/rc.d/init.d/mysqld restart;/etc/rc.d/init.d/httpd restart";
+        system("bash -c '$cmd'");
+}
+else
+{
+        print "more than 500000";;
+
+}
+
